64a6245efb01ff956b16fb88374b258ccfee42cb
Lucas Russo <lerwys@gmail.com>
2017-11-23 15:38:25 -0200

    {scripts,systemd}: add systemd integration scripts

---
+++ systemd/bin/stopDockerDMM7510.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [ -z "$DMM7510_INSTANCE" ]; then
+    echo "Device type is not set. Please use -d option" >&2
+    exit 1
+fi
+
+/usr/bin/docker stop \
+    dmm7510-epics-ioc-${DMM7510_INSTANCE}
